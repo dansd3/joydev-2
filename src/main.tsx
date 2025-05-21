2b9d7922cf2a00e56593d1d8cf9af4f800c89d5d
dansd3 <115929379+dansd3@users.noreply.github.com>
2025-05-21 16:19:04 +0300

    fix(toasts): Resolve review issues
    Issues fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import './styles/index.scss';
 import './assets/fonts/fonts.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App/App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
